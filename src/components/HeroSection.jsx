066a03c6ccb1b6515ffd5756e9c3629ada081477
Alfonso Dalix <50084031+Alfondalix@users.noreply.github.com>
2020-09-28 18:04:44 -0300

    Homepage done, need to adjust footer

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
@@ -24,7 +24,6 @@ function HeroSection() {
         >
           WATCH TRAILER <i className="far fa-play-circle" />
         </Button>
-        7
       </div>
     </div>
   );
